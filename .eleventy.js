546f3e58bdafe4543cd8a859b4fe6c3386de99bb
Joshua Dors <73672400+joshua-dors@users.noreply.github.com>
2023-03-04 19:16:22 -0500

    Add tailwind css

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,4 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addWatchTarget("./src/css/");
-
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-
   return {
     dir: {
       input: "src",
--- src/pages/projects.md
+++
@@ -1,8 +0,0 @@
----
-title: Projects
----
-
-## Projects
-
-- [**Spark**](https://duckduckgo.com) (**In progress**) Clean, simple, and easy to use electric vehicle web and mobile app.
-- More coming soon!
